a5876a9803809540ffe19c55d1bd66631fdced7c
ovas04 <oca0403@gmail.com>
2022-07-21 03:20:45 -0500

    added tests

--- src/test/java/com/springservices/SpringservicesApplicationTests.java
+++ src/test/java/com/springservices/SpringservicesApplicationTests.java
@@ -1,7 +1,12 @@
 package com.springservices;
 
+import com.springservices.services.PersonaServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class SpringservicesApplicationTests {
@@ -10,4 +15,34 @@ class SpringservicesApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    public void test1(){
+
+        Persona persona = new Persona("Roberto", "Abierto");
+
+        Assertions.assertEquals(persona.getName(),"Roberto");
+
+
+    }
+
+    @Test
+    public void test2(){
+
+        PersonaServiceImpl personaService = new PersonaServiceImpl();
+
+        final List<Persona> list = personaService.listar();
+
+        Assertions.assertEquals(((Object) list).getClass().getSimpleName(), ArrayList.class.getSimpleName());
+
+    }
+
+    @Test
+    public void test3(){
+
+        Persona persona = new Persona();
+
+        Assertions.assertNotEquals(persona.getName(), "Roberto");
+
+
+    }
 }
